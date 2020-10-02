92e2392bb9e8a4e530b0b99de31c553f4c068926
Blade-2020 <al.almaktoum@live.com>
2020-10-02 11:33:07 +1000

    Updates

---
+++ precache-manifest.3ebfe8c395352904e17fcd9b2ae834f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26f5068571da5139a7becf59445b8cf",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "65dec083274ed90b89cd",
+    "url": "/github-finder/static/css/main.5d8560e9.chunk.css"
+  },
+  {
+    "revision": "8ae4ca209c940a3523d4",
+    "url": "/github-finder/static/js/2.8a63fced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder/static/js/2.8a63fced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65dec083274ed90b89cd",
+    "url": "/github-finder/static/js/main.cadfc360.chunk.js"
+  },
+  {
+    "revision": "189fbc883acbcb16644e",
+    "url": "/github-finder/static/js/runtime-main.c7dda347.js"
+  }
+]);
